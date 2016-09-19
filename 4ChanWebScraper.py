085cddee10e8b1374f8b0c2e2a1f9bd6c9feb486
Anthony Woody <powderedeggs@gmail.com>
2016-09-19 00:57:49 -0400

    regex broke, added file existing checks, added switch to not save

--- 4ChanWebScraper.py
+++ 4ChanWebScraper.py
@@ -32,7 +32,7 @@ def createFolderName(webpageTitle):
     return outputString
 
 def validateUrl(url):
-    pattern = r'^(https?://)?(www\.)?4chan\.'
+    pattern = r'^(https?://)?(www\.)?(boards\.)?4chan\.'
     if( re.match(pattern, url, re.I)):
         return url
     else:
@@ -46,11 +46,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process a 4chan thread to scrape all images from.')
     parser.add_argument('url',nargs='+', type=validateUrl, help='a list, separated by spaces, of web addresses to be parsed')
     parser.add_argument('-v', help='verbose. Turn on debug output.', action='store_true', dest='verbose')
+    parser.add_argument('-ns', help='No saving. This disables actually modifying the filesystem.', action='store_false', dest='save')
     args = parser.parse_args()
 
     from BeautifulSoup import BeautifulSoup
     import requests
     from StringIO import StringIO
+    import os
 
     for link in args.url:
         if args.verbose:
@@ -67,12 +69,39 @@ if __name__ == '__main__':
         if args.verbose:
             print('Capturing ' + folderName)
         
-        for link in soup.findAll('a', 'fileThumb'):
-            imageName = link.get('href')
+        for link in soup.findAll('div','fileText'): 
+            imageName = link.a.get('href')
             if args.verbose:
-                print('Getting ' + imageName)
-            fileName = re.search('\d+\.\w+$',imageName)
-            savePath = './'+ folderName +'/' + fileName.group(0)
-            if args.verbose:
-                print('saving:' + savePath)
-            downloadImage('http:'+imageName,savePath)
+                print('Processing ' + imageName)
+            title = link.a.get('title')
+            #save the title as the filename
+            if title is not None:
+                savePath = './'+folderName+'/'+title
+                if args.verbose:
+                    print('Title is found. Using it as filename; ' + title)
+                if not os.path.isfile(savePath):
+                    if args.verbose:
+                        print('Saving from http:' + imageName)
+                        print('No previous file exists. Saving to ' + savePath)
+                        print('Saving files: ' + str(args.nosave) )
+                    if args.save:
+                        downloadImage('http:'+imageName,savePath)
+                else:
+                    print('File exists. Skipping it.')
+
+            #save the filename as the filename 
+            else:
+                fileName = re.search('\d+\.\w+$',imageName).group(0)
+                if args.verbose:
+                    print('Title not found. Using filepath as filename;  ' + fileName)
+                savePath = './'+ folderName +'/' + fileName
+                if args.verbose:
+                    print('saving:' + savePath)
+                if not os.path.isfile(savePath):
+                    if args.save:
+                        downloadImage('http:'+imageName,savePath)
+                else:
+                    if args.verbose:
+                        print(fileName+ ' found. Skipping.')
+        
+        print('Finished processing thread!')
